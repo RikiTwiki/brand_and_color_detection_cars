ec56d0bee126f98f8e816cf8c9e5bbd8aa55f47f
rikitwiki <aibekrenadov@gmail.com>
2024-01-05 18:01:42 +0600

    save

--- kok.py
+++ kok.py
@@ -15,14 +15,7 @@ from PIL import Image
 data = ImageDataLoaders.from_folder('../imgs_zip/imgs/train', train='.', valid_pct=0.2, 
                                    size=224, item_tfms=Resize(224))
 
-learn = cnn_learner(data, models.resnet18, model_dir='')
-
-learn.export(Path("/home/sanarip03/Desktop/бренд_машин/Image-Classification-using-fastai-main/export.pkl"))
-learn.model_dir = ""
-
 learn = load_learner('export_v2.pkl')
-
-learn.load("stage-1_v2")
 
 def predict_image(image_path):
     # Open the image
